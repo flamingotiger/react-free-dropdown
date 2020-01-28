ba82d40d0e881c1f0067a5d63713a885dc10f8e4
flamingotiger <hhbhong@gmail.com>
2020-01-28 14:24:29 +0900

    Add ul component

--- src/Fdd.tsx
+++ src/Fdd.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useEffect } from 'react';
 import styled from 'styled-components';
 
 interface FDDType {
@@ -7,7 +7,7 @@ interface FDDType {
 }
 
 interface FDDPropsType extends FDDType {
-
+    children?: React.ReactNode;
 }
 
 interface FDDOptionType extends FDDType {
@@ -18,39 +18,63 @@ interface FDDSelectType extends FDDType {
 
 }
 
-const FDDSelectStyle = {
-    Wrapper: styled.div`
-    width: 100px;
-    height: 20px;
-    border: 2px solid blue;
-    `
-}
 const FDDOptionStyle = {
     Wrapper: styled.div`
-    width: 100px;
-    height: 20px;
-    border: 2px solid green;
+        width: 100%;
+        border: 2px solid green;
+        cursor: pointer;
+        list-style: none;
+        padding: 4px;
+        box-sizing: border-box;
+    `
+}
+const FDDSelectStyle = {
+    Wrapper: styled.div`
+        border: 2px solid blue;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        padding: 4px;
     `
 }
 const FDDStyle = {
     Wrapper: styled.div`
-    width: 200px;
-    height: 100px;
-    border: 10px solid red;
-`
+        display: inline-block;
+        border: 10px solid red;
+        position: relative;
+    `,
+    Ul: styled.ul`
+        display: block;
+        list-style: none;
+        position: absolute;
+        border: 10px solid yellow;
+        left: 0;
+        top: 100%;
+        margin: 0;
+        padding: 0;
+        width: ${(props: { width: number }) => `${props.width}px`}
+    `
 }
 
 export const FDDOption: React.FC<FDDOptionType> = props => {
     return <FDDOptionStyle.Wrapper>Option</FDDOptionStyle.Wrapper>
 }
 
-const FDDSelect: React.FC<FDDSelectType> = props => {
-    return <FDDSelectStyle.Wrapper>FDDSelect</FDDSelectStyle.Wrapper>
-}
-
 const Fdd: React.FC<FDDPropsType> = props => {
+    const {children, className, style} = props;
+    const [selectWidth, setSelectWidth] = React.useState<number>(0);
+    const selectEl = React.useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (selectEl && selectEl.current) {
+            const {width} = selectEl.current.getBoundingClientRect();
+            setSelectWidth(width);
+        }
+    }, [selectEl]);
     return (<FDDStyle.Wrapper>
-        <FDDSelect/>
+        <FDDSelectStyle.Wrapper className={className} style={style} ref={selectEl}>셀렉트</FDDSelectStyle.Wrapper>
+        <FDDStyle.Ul width={selectWidth}>
+            {children}
+        </FDDStyle.Ul>
     </FDDStyle.Wrapper>)
 }
 
--- src/example/index.tsx
+++ src/example/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import Fdd from "../Fdd";
+import Fdd, {FDDOption} from "../Fdd";
 
 const Example: React.FC = () => {
-    return <Fdd/>
+    return (
+        <>
+            <Fdd>
+                <FDDOption>test1</FDDOption>
+                <FDDOption>test2</FDDOption>
+                <FDDOption>test3</FDDOption>
+                <FDDOption>test4</FDDOption>
+            </Fdd>
+        </>)
 }
 
 export default Example;
