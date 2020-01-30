d9a0b9120ce3d27f20b1c9c29638c6475d04280d
flamingotiger <hhbhong@gmail.com>
2020-01-30 14:48:09 +0900

    Fix example

--- src/example/index.tsx
+++ src/example/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Fdd, {FDDOption} from "../Fdd";
+import Fdd, { FDDOption } from "../Fdd";
 
 const Example: React.FC = () => {
     return (
         <>
             <Fdd>
-                <FDDOption>test1</FDDOption>
-                <FDDOption>test2</FDDOption>
-                <FDDOption>test3</FDDOption>
-                <FDDOption>test4</FDDOption>
+                <FDDOption value={1}>test1</FDDOption>
+                <FDDOption value={2}>test2</FDDOption>
+                <FDDOption value={3}>test3</FDDOption>
+                <FDDOption value={4}>test4</FDDOption>
             </Fdd>
         </>)
 }
