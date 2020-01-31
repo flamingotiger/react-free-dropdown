41b4cce0546393b192d2e8f94c1b3bf8c3049b31
flamingotiger <hhbhong@gmail.com>
2020-01-31 14:48:28 +0900

    Add option and select style

--- src/Fdd.tsx
+++ src/Fdd.tsx
@@ -11,53 +11,70 @@ interface FDDPropsType extends FDDType {
 }
 
 interface FDDOptionType extends FDDType {
-
-}
-
-interface FDDSelectType extends FDDType {
-
+    value: string | number;
 }
 
 const FDDOptionStyle = {
     Wrapper: styled.div`
         width: 100%;
-        border: 2px solid green;
         cursor: pointer;
         list-style: none;
-        padding: 4px;
+        padding: 4px 4px 4px 8px;
         box-sizing: border-box;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        font-size: 14px;
+        line-height: 14px;
+        color: white;
+        &:hover{
+          background-color: #00bfff; 
+        }
     `
 }
 const FDDSelectStyle = {
     Wrapper: styled.div`
-        border: 2px solid blue;
         display: flex;
         align-items: center;
         cursor: pointer;
         padding: 4px;
+        box-sizing: border-box;
+        background: white;
+        border-radius: 4px;
+        font-size: 14px;
+        line-height: 14px;
+        border: 1px solid rgba(255,255,255,0);
+        &:hover{
+          border: 1px solid #00bfff;
+        }
     `
 }
 const FDDStyle = {
     Wrapper: styled.div`
         display: inline-block;
-        border: 10px solid red;
         position: relative;
+        box-sizing: border-box;
     `,
     Ul: styled.ul`
         display: block;
         list-style: none;
         position: absolute;
-        border: 10px solid yellow;
         left: 0;
         top: 100%;
         margin: 0;
         padding: 0;
-        width: ${(props: { width: number }) => `${props.width}px`}
+        width: ${(props: { width: number }) => `${props.width}px`};
+        box-sizing: border-box;        
+        border-radius: 4px;
+        background: rgba(100, 100, 100, 0.8);
+        overflow: hidden;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     `
 }
 
 export const FDDOption: React.FC<FDDOptionType> = props => {
-    return <FDDOptionStyle.Wrapper>Option</FDDOptionStyle.Wrapper>
+    const {value, children} = props;
+    return <FDDOptionStyle.Wrapper>{children}</FDDOptionStyle.Wrapper>
 }
 
 const Fdd: React.FC<FDDPropsType> = props => {
@@ -71,10 +88,12 @@ const Fdd: React.FC<FDDPropsType> = props => {
         }
     }, [selectEl]);
     return (<FDDStyle.Wrapper>
-        <FDDSelectStyle.Wrapper className={className} style={style} ref={selectEl}>셀렉트</FDDSelectStyle.Wrapper>
+        <FDDSelectStyle.Wrapper className={className} style={style} ref={selectEl}>Select...</FDDSelectStyle.Wrapper>
+        {children &&
         <FDDStyle.Ul width={selectWidth}>
+          <FDDOption value=''>Select...</FDDOption>
             {children}
-        </FDDStyle.Ul>
+        </FDDStyle.Ul>}
     </FDDStyle.Wrapper>)
 }
 
