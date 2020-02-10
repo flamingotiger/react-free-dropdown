a457e87ad8c2eb4a947405b1db1aed83ec87e25a
flamingotiger <hhbhong@gmail.com>
2020-02-10 16:01:35 +0900

    Add cypress test

---
+++ cypress/integration/react-free-dropdown/1-custom-style_test.js
@@ -0,0 +1,99 @@
+describe('custom-style-test', () => {
+	it('custom-style toggle', () => {
+		cy.get('#explorercustom-style').click();
+	});
+	it('active options custom select and option style category', () => {
+		cy.get('#explorercustom-style--custom-select-style-and-option-style').click();
+	});
+	it('active options custom select and option style', () => {
+		cy.get('#storybook-preview-iframe').then($iframe => {
+			const $body = $iframe.contents().find('body');
+			const optionSelect = name => {
+				cy.wrap($body)
+					.find('#select')
+					.click();
+				cy.wrap($body)
+					.find('#list')
+					.contains(name)
+					.click();
+			};
+			optionSelect('Apple');
+			optionSelect('Carrot');
+			optionSelect('Banana');
+			optionSelect('Grape');
+			optionSelect('Orange');
+		});
+	});
+	it('hover custom style category', () => {
+		cy.get('#explorercustom-style--custom-hover-style').click();
+	});
+	it('hover custom style', () => {
+		cy.get('#storybook-preview-iframe').then($iframe => {
+			const $body = $iframe.contents().find('body');
+			cy.wrap($body)
+				.find('#select')
+				.click();
+			const optionSelect = name => {
+				cy.wrap($body)
+					.find('#list')
+					.contains(name)
+					.trigger('mouseover');
+			};
+			optionSelect('Apple');
+			optionSelect('Carrot');
+			optionSelect('Banana');
+			optionSelect('Grape');
+			cy.wrap($body)
+				.find('#list')
+				.contains('Orange')
+				.click();
+		});
+	});
+	it('each option style custom category', () => {
+		cy.get('#explorercustom-style--each-custom-option-style').click();
+	});
+	it('each option style custom', () => {
+		cy.get('#storybook-preview-iframe').then($iframe => {
+			const $body = $iframe.contents().find('body');
+			const optionSelect = name => {
+				cy.wrap($body)
+					.find('#select')
+					.click();
+				cy.wrap($body)
+					.find('#list')
+					.contains(name)
+					.click();
+			};
+			optionSelect('Apple');
+			optionSelect('Carrot');
+			optionSelect('Banana');
+			optionSelect('Grape');
+			optionSelect('Orange');
+		});
+	});
+	it('custom style category', () => {
+		cy.get('#explorercustom-style--custom-style').click();
+	});
+	it('custom style', () => {
+		cy.get('#storybook-preview-iframe').then($iframe => {
+			const $body = $iframe.contents().find('body');
+			cy.wrap($body)
+				.find('#select')
+				.click();
+			const optionSelect = name => {
+				cy.wrap($body)
+					.find('#list')
+					.contains(name)
+					.trigger('mouseover');
+			};
+			optionSelect('Apple');
+			optionSelect('Carrot');
+			optionSelect('Banana');
+			optionSelect('Grape');
+			cy.wrap($body)
+				.find('#list')
+				.contains('Orange')
+				.click();
+		});
+	});
+});
